20414d57e298e0e2712c328e20da5ca2affeb6ce
yuta.onishi <rad6242@icloud.com>
2022-01-26 14:47:06 +0900

    feat: usersコントローラーのcreateアクションを設定
    userを作成できるようにするため、アクションを設定しました。

--- api/app/controllers/api/v1/users_controller.rb
+++ api/app/controllers/api/v1/users_controller.rb
@@ -7,6 +7,21 @@ module Api
           users: users
         }, status: :ok
       end
+
+      def create
+        user = User.new(user_params)
+        if user.save
+          render json: {}, status: :created
+        else
+          render json: {}, status: :bad_request
+        end
+      end
+
+      private
+
+      def user_params
+        params.require(:user).permit(:name, :email, :password, :password_confirmation)
+      end
     end
   end
 end
