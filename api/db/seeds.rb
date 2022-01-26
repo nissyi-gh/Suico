26e5bfa4bae4ccf7d856b80a0b316e5a714e7738
yuta.onishi <rad6242@icloud.com>
2022-01-26 15:49:24 +0900

    feat: seedにパスワードを持たせました
    password追加に伴いパスワードを持たせました。

--- api/db/seeds.rb
+++ api/db/seeds.rb
@@ -1,13 +1,17 @@
 # 管理者ユーザーを登録
 User.create!(
   name: "admin user",
-  email: "admin.user@test.com"
+  email: "admin.user@test.com",
+  password: "adminuser",
+  password_confirmation: "adminuser",
 )
 
 # 一般テストユーザーを登録
 1.upto(10) do |n|
   User.create!(
     name: "#{n}th user",
-    email: "test.user@#{n}th.com"
+    email: "test.user@#{n}th.com",
+    password: "testpass",
+    password_confirmation: "testpass"
   )
 end
