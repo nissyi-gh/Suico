20790a238fe84e278cab65112ebac557b7948478
yuta.onishi <rad6242@icloud.com>
2022-01-31 15:42:00 +0900

    feat: 特定ユーザーのSleepLogを返す処理を実装。
    ユーザー1の睡眠データ、平均などを返す処理を実装しました。

--- api/app/controllers/api/v1/sleep_logs_controller.rb
+++ api/app/controllers/api/v1/sleep_logs_controller.rb
@@ -2,8 +2,25 @@ module Api
   module V1
     class SleepLogsController < ApplicationController
       def index
-        logs = SleepLog.all
-        render json: { logs: logs }, status: :ok
+        # Rails側ではUTCで保存、出力する
+        # 文字列で出力する場合には、JSTに変換したうえでフロントに渡す
+
+        # テスト用にuser_id: 1のデータのみ表示
+        sleep_logs = SleepLog.select(:id, :sleep_at, :wake_at, :satisfaction).where(user_id: 1)
+        # sleep_logs = SleepLog.select(:id, :sleep_at, :wake_at, :satisfaction).where(user_id: current_user.id)
+        sleep_data = sleep_logs.sleep_log_index(sleep_logs)
+
+        render json: {
+          sleep_logs: sleep_logs,
+          average: {
+            sleep_at: sleep_data[0],
+            wake_at: sleep_data[1],
+            satisfaction: sleep_data[2],
+            sleep_time: sleep_data[3]
+          },
+          max: sleep_data[4],
+          min: sleep_data[5]
+        }, status: :ok
       end
     end
   end
--- api/app/models/sleep_log.rb
+++ api/app/models/sleep_log.rb
@@ -4,4 +4,73 @@ class SleepLog < ApplicationRecord
   default_scope { order(created_at: :desc) }
   validates :sleep_at, presence: true
   validates :wake_at, presence: true
+
+  class << self
+    def sleep_log_analyze(logs)
+      sleep_at_average = time_average(logs, :sleep_at)
+      wake_at_avereage = time_average(logs, :wake_at)
+
+      [sleep_at_average, wake_at_avereage]
+    end
+
+    def time_average(model, target)
+      # UTCとJSTの時差540分。RailsはUTC、文字列での出力時にはJSTに変換する
+      time_difference_between_utc_and_jst = 540
+      # 24時台は0時扱いになってしまう。1440秒足すことで24時台として計算する
+      add_24_hour = 60 * 24
+      times = model.pluck(target).map do |log|
+        log.hour.zero? ? (log.hour * 60) + log.min + add_24_hour : (log.hour * 60) + log.min
+      end
+      time_average = (times.sum / model.size) + time_difference_between_utc_and_jst
+      # 24時間を超えている場合は、24時間をマイナス
+      time_average -= (24 * 60) if time_average > (24 * 60)
+      format('%02<hour>d:%02<min>d', hour: time_average / 60, min: time_average % 60)
+    end
+
+    # 睡眠時間を計算する。
+    def sleep_time(sleep_logs)
+      sleep_logs.each do |log|
+        log.sleep_time = Time.at(log[:wake_at] - log[:sleep_at]).utc.strftime('%H:%M')
+      end
+    end
+
+    def sleep_time_analyze(logs)
+      sleep_times = []
+      logs.each do |log|
+        sleep_times << (log[:wake_at] - log[:sleep_at])
+      end
+      average = sleep_time_average(sleep_times, logs.size)
+      max = Time.at(sleep_times.max).utc.strftime('%H:%M')
+      min = Time.at(sleep_times.min).utc.strftime('%H:%M')
+
+      [average, max, min]
+    end
+
+    def sleep_time_average(sleep_times, size)
+      time_average = sleep_times.sum / size / 60
+      format('%02<hour>d:%02<min>d', hour: time_average / 60, min: time_average % 60)
+    end
+
+    # 満足度の平均をnull以外から算出。
+    def satisfaction_average(sleep_logs)
+      satisfactions = sleep_logs.pluck(:satisfaction)
+      null_count = 0
+      satisfactions_total = 0
+      satisfactions.each do |item|
+        item ? satisfactions_total += item : null_count += 1
+      end
+      (satisfactions_total / (sleep_logs.size - null_count).to_f).round(1)
+    end
+
+    # indexアクションの処理をまとめる
+    def sleep_log_index(sleep_logs)
+      # 各データに睡眠時間をセット
+      sleep_time(sleep_logs)
+      # 起床時間、就寝時間の平均を取得
+      data = sleep_log_analyze(sleep_logs)
+      # 睡眠時間、最長睡眠、最短睡眠を取得
+      sleep_time_data = sleep_time_analyze(sleep_logs)
+      [*data, satisfaction_average(sleep_logs), *sleep_time_data]
+    end
+  end
 end
