d67d63603cab2743e98e34ae759fb52954923604
yuta.onishi <rad6242@icloud.com>
2022-01-31 15:37:37 +0900

    feat: validation, 依存関係などを追加
    UserとSleepLogに追加しました。

--- api/app/models/sleep_log.rb
+++ api/app/models/sleep_log.rb
@@ -1,2 +1,7 @@
 class SleepLog < ApplicationRecord
+  attribute :sleep_time
+  belongs_to :user
+  default_scope { order(created_at: :desc) }
+  validates :sleep_at, presence: true
+  validates :wake_at, presence: true
 end
