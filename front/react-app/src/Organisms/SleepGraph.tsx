1658cd1ab7c1bec4931ed85468652aff3c88b0ab
yuta.onishi <rad6242@icloud.com>
2022-02-02 11:51:29 +0900

    style: グラフのスタイル変更
    LineChartの太さを変更しました。

--- front/react-app/src/Organisms/SleepGraph.tsx
+++ front/react-app/src/Organisms/SleepGraph.tsx
@@ -33,13 +33,13 @@ export const SleepGraph = memo(({ sleepLogs } : { sleepLogs: SleepLogListItem[] 
       >
         <CartesianGrid strokeDasharray="3 3" />
         <XAxis dataKey="date" />
-        <YAxis yAxisId={1} label={{ value: "時刻", angle: -90, dx: -12 }} />
+        <YAxis yAxisId={1} label={{ value: "時刻", angle: -90, dx: -12 }} domain={[2, 26]}/>
         <YAxis yAxisId={2} orientation="right" label={{ value: "満足度", angle: -90, dx: 12 }} domain={[0, 5]} />
         <Tooltip />
         <Legend />
-        <Line type="monotone" yAxisId={1} dataKey="起床時刻" stroke="#8884d8" activeDot={{ r: 8 }} />
-        <Line type="monotone" yAxisId={1} dataKey="就寝時刻" stroke="#fcca9d" />
-        <Line type="monotone" yAxisId={2} dataKey="満足度" stroke="#82ca9d" />
+        <Line type="monotone" yAxisId={2} dataKey="満足度" stroke="#82ca9d" strokeWidth={1.5} />
+        <Line type="monotone" yAxisId={1} dataKey="起床時刻" stroke="#8884d8" activeDot={{ r: 8 }} strokeWidth={3} />
+        <Line type="monotone" yAxisId={1} dataKey="就寝時刻" stroke="#fcca9d"  strokeWidth={3} />
       </LineChart>
     </ResponsiveContainer>
   );
