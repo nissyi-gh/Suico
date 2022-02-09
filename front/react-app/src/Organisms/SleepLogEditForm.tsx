8796085b13911b7e2466ae49382e6b9f96a6ab55
yuta.onishi <rad6242@icloud.com>
2022-02-09 15:22:54 +0900

    feat: 編集フォームで編集したことを検知
    睡眠ログを編集したことを検知できるようにしました。

--- front/react-app/src/Organisms/SleepLogEditForm.tsx
+++ front/react-app/src/Organisms/SleepLogEditForm.tsx
@@ -1,11 +1,40 @@
-
+import { useEffect, useState } from "react";
 import { AlarmSetterWithLabel, SatisfactionSelectorWithLabel, submitButton } from "../Molecules/Form";
 import { ModalClose } from "../Molecules/ModalClose";
 import { SleepLogListItem } from "../types/types";
 
 export const SleepLogEditForm = ({ hideModalFunction, log } : { hideModalFunction: () => void, log: SleepLogListItem }): JSX.Element => {
+  const [beforeLog] = useState<SleepLogListItem>(log);
+  const [changeLog, setChangeLog] = useState<SleepLogListItem>(log);
+
+  useEffect(() => {
+    console.log("送信します")
+  }, [beforeLog, changeLog])
+
   const onClickSubmit = (e: React.FormEvent<HTMLInputElement>): void => {
+    const wakeAtHour = (document.getElementById('wakeAt_hour') as HTMLSelectElement).value;
+    const wakeAtMin = (document.getElementById('wakeAt_min') as HTMLSelectElement).value;
+    const sleepAtHour = (document.getElementById('sleepAt_hour') as HTMLSelectElement).value;
+    const sleepAtMin = (document.getElementById('sleepAt_min') as HTMLSelectElement).value;
+    const satisfaction = (document.getElementById('satisfaction') as HTMLSelectElement).value;
     e.preventDefault();
+
+    const changingLog: SleepLogListItem = {
+      sleepLogId: log.sleepLogId,
+      wakeAt: log.wakeAt.hour(parseInt(wakeAtHour, 10)).minute(parseInt(wakeAtMin, 10)),
+      sleepAt: log.sleepAt.hour(parseInt(sleepAtHour, 10)).minute(parseInt(sleepAtMin, 10)),
+      sleepTime: log.sleepTime,
+      satisfaction: parseInt(satisfaction, 10)
+    }
+
+    if ((beforeLog.wakeAt.isSame(changingLog.wakeAt)) &&
+        (beforeLog.sleepAt.isSame(changingLog.sleepAt)) && 
+        (beforeLog.satisfaction === changingLog.satisfaction))
+    {
+      return console.log("変更なし")
+    }
+
+    setChangeLog(changingLog);
   }
 
   return(
