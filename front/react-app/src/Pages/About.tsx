cc018fb54eb2918bc2ced4b8605744ab1f7e9a9f
yuta.onishi <rad6242@icloud.com>
2022-02-12 00:58:36 +0900

    style: テキストコンテンツのスタイルづけを修正
    divによるスタイルづけを統一しました。

--- front/react-app/src/Pages/About.tsx
+++ front/react-app/src/Pages/About.tsx
@@ -3,7 +3,7 @@ import { MainContentInner } from "../Templates/MainContentInner"
 
 const AboutContent = (): JSX.Element => {
   return (
-    <div className="w-2/3 h-full mx-auto overflow-y-scroll pt-8">
+    <>
       { AboutSection("Suicoとは?", 
           "睡眠から生活を良くしたい、そういった思いでこのアプリを作成しました。「睡（すい）眠から生活や人生を向（こう）上させる」という思いを込めてSuicoと名付けました。"
       )}
@@ -21,7 +21,7 @@ const AboutContent = (): JSX.Element => {
       )}
       { AboutSection("睡眠の大事さ", "作者は以前、鉄道会社で運転士として勤務していました。鉄道会社の勤務は泊まりを伴う長時間勤務で、終電や始発から電車を動かすために3〜4時間の仮眠をとり、睡眠不足に耐えながら安全輸送を完遂する必要がありました。")}
       <img src="sheep_dark.png" alt="Sheep"/>
-    </div>
+    </>
   )
 }
 
