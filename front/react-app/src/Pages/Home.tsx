bc59f8391699b3542fe33d1caf7f131285a9f846
yuta.onishi <rad6242@icloud.com>
2022-02-12 16:16:22 +0900

    style: Home画面の修正 #22
    ログインを促せるようにStyle付を変更しました。

--- front/react-app/src/Pages/Home.tsx
+++ front/react-app/src/Pages/Home.tsx
@@ -3,10 +3,10 @@ import { MainContentInner } from "../Templates/MainContentInner"
 
 const HomeContent = (): JSX.Element => {
   return <>
+    <p className="border-2 border-red-300 bg-red-50 rounded-md mb-8 p-4">上記コンテンツのアクセスにはログインが必要です。ページ上部からログイン、または新規登録をお願いします。</p>
     {
       AboutSection("Welcome to Suico", "Suicoは睡眠を記録・管理するためWebアプリケーションです。睡眠を記録してグラフ化したり、アラームプリセットを作成することができます。")
     }
-    <p className="border border-red-500">上記コンテンツのアクセスにはログインが必要です。ページ上部からログイン、または新規登録をお願いします。</p>
   </>
 }
 
