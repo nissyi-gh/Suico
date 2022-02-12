1699d7378f0e7a14e7622cca7ac633aa346c17c8
yuta.onishi <rad6242@icloud.com>
2022-02-12 15:48:24 +0900

    fix: RouteからLoginCheckの呼び出し方を変更
    関数の呼び出し方に変更しました。

--- front/react-app/src/Pages/Home.tsx
+++ front/react-app/src/Pages/Home.tsx
@@ -6,6 +6,7 @@ const HomeContent = (): JSX.Element => {
     {
       AboutSection("Welcome to Suico", "Suicoは睡眠を記録・管理するためWebアプリケーションです。睡眠を記録してグラフ化したり、アラームプリセットを作成することができます。")
     }
+    <p className="border border-red-500">上記コンテンツのアクセスにはログインが必要です。ページ上部からログイン、または新規登録をお願いします。</p>
   </>
 }
 
