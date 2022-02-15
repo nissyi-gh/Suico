dfd82610cd30a25deb9de9347bba219229fb2ec3
yuta.onishi <rad6242@icloud.com>
2022-02-16 00:19:09 +0900

    fix: 平均データのロジックとseedデータの修正
    平均が正しく表示されないバグを修正しました。
    seedデータで生成するデータの種類を増やしました。

--- api/db/seeds.rb
+++ api/db/seeds.rb
@@ -23,26 +23,50 @@ User.create!(
   )
 end
 
+# 就寝と起床が同日になる睡眠データを生成
+def same_day_sleep_log_creater(user_id, today, iterator, satisfactions)
+  SleepLog.create(
+    user_id: user_id,
+    sleep_at: Time.local(today.year, today.month, today.day, rand(0..1), rand(0..59), 0).in_time_zone - iterator.day,
+    wake_at: Time.local(today.year, today.month, today.day, rand(5..9), rand(0..59), 0).in_time_zone - iterator.day,
+    satisfaction: satisfactions[rand(6)]
+  )
+end
+
+# 就寝と起床が別日になる睡眠データを生成
+def diff_day_sleep_log_creater(user_id, today, iterator, satisfactions)
+  SleepLog.create(
+    user_id: user_id,
+    sleep_at: Time.local(today.year, today.month, today.day, rand(21..23), rand(0..59), 0).in_time_zone - iterator.day,
+    wake_at: Time.local(today.year, today.month, today.day, rand(5..9), rand(0..59), 0).in_time_zone - iterator.day + 1.day,
+    satisfaction: satisfactions[rand(6)]
+  )
+end
+
 # 睡眠データを登録
 def make_sleep_logs(times, user_id)
   t = Time.zone.now
   satisfactions = [nil, 0.0, 1.25, 2.5, 3.75, 5.0]
-  sleep_in_hour = [22, 23, 0, 1]
 
   times.downto(1) do |i|
-    SleepLog.create(
-      user_id: user_id,
-      sleep_at: Time.local(t.year, t.month, (t - i).day, sleep_in_hour[rand(0..3)], rand(0..59), 0).in_time_zone,
-      wake_at: Time.local(t.year, t.month, (t - i + 1).day, rand(6..10), rand(0..59), 0).in_time_zone,
-      satisfaction: satisfactions[rand(6)]
-    )
+    # 0の場合のみ、就寝、起床ともに日付は同じ（夜ふかしパターン）。
+    create_mode = rand(10)
+
+    case create_mode
+    when 0
+      # 日付は同じ（夜ふかしパターン）
+      same_day_sleep_log_creater(user_id, t, i, satisfactions)
+    else
+      # 日付はまたぐ（早寝早起きパターン）
+      diff_day_sleep_log_creater(user_id, t, i, satisfactions)
+    end
   end
 end
 
 # 管理者
-make_sleep_logs(10, 1)
+make_sleep_logs(50, 1)
 # ゲスト
-make_sleep_logs(10, 2)
+make_sleep_logs(50, 2)
 
 # ゲストのみ睡眠プリセットを登録
 1.upto(20) do |i|
