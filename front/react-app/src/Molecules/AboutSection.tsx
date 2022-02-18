b86ca8fba546af08afc71bf51281d029e984b004
yuta.onishi <rad6242@icloud.com>
2022-02-18 10:53:03 +0900

    style: 「サイトについて」のスタイル付

--- front/react-app/src/Molecules/AboutSection.tsx
+++ front/react-app/src/Molecules/AboutSection.tsx
@@ -2,8 +2,8 @@ import { AboutParagraphAtom } from "../Atoms/AboutParagraph"
 
 export const AboutSection = (sectionTitle: string, content: string ) => {
   return (
-    <section className="mb-8">
-      <h1 className="text-lg border-b-2 border-b-black">{ sectionTitle }</h1>
+    <section className="mb-16 leading-relaxed">
+      <h1 className="text-xl border-b-2 mb-2 pb-2 border-b-black dark:border-b-gray-200">{ sectionTitle }</h1>
       { AboutParagraphAtom(content)}
     </section>
   )
