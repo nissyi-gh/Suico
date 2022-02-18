645a45b3dbf5058cd900b19a72e32d0690d20da5
yuta.onishi <rad6242@icloud.com>
2022-02-18 11:58:42 +0900

    style: Home画面のstyle適用

--- front/react-app/src/Pages/Home.tsx
+++ front/react-app/src/Pages/Home.tsx
@@ -3,9 +3,9 @@ import { MainContentInner } from "../Templates/MainContentInner"
 
 const HomeContent = (): JSX.Element => {
   return <>
-    <p className="border-2 border-red-300 bg-red-50 rounded-md mb-8 p-4">上記コンテンツのアクセスにはログインが必要です。ページ上部からログイン、または新規登録をお願いします。</p>
+    <p className="border-2 border-red-300 dark:border-gray-200 bg-red-50 dark:bg-gray-500 rounded-md mb-8 p-4">上記コンテンツのアクセスにはログインが必要です。ページ上部からログイン、または新規登録をお願いします。</p>
     {
-      AboutSection("Welcome to Suico", "Suicoは睡眠を記録・管理するためWebアプリケーションです。睡眠を記録してグラフ化したり、アラームプリセットを作成することができます。")
+      AboutSection("Welcome to Suico", "Suicoは睡眠を記録・管理するためWebアプリケーションです。睡眠を記録して可視化したり、アラームプリセットを作成することができます。睡眠の改善にお役立てください。")
     }
   </>
 }
