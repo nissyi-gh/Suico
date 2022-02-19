05c57d315861d13871a7e9399665570977df4a31
yuta.onishi <rad6242@icloud.com>
2022-02-19 19:18:44 +0900

    feat: fixedTabからアラーム画面を表示
    モバイル表示時にアラーム画面を開けるようにしました。

--- front/react-app/src/Organisms/FixedTab.tsx
+++ front/react-app/src/Organisms/FixedTab.tsx
@@ -1,18 +1,30 @@
 import { Link } from "react-router-dom";
 import { GiNotebook } from 'react-icons/gi';
 import { BsAlarm } from 'react-icons/bs';
+import { sleepLogsURL } from "../constants/urls";
+import { useState } from "react";
+import { AlarmModal } from '../Pages/AlarmModal';
 
 export const FixedTab = (): JSX.Element => {
+  const [isAlarmShow, setIsAlarmShow] = useState<boolean>(false);
+
+  const hiddenAlarmModal = (): void => {
+    setIsAlarmShow(false);
+  }
+
   return (
     <div className="md:hidden fixed flex bottom-0 left-0 h-16 w-full z-10 bg-sky-200 dark:bg-gray-700 text-center text-xs border-t border-gray-400">
-      <Link to="#" className="h-full w-1/2 p-2">
+      <Link to={ sleepLogsURL } className="h-full w-1/2 p-2">
         <GiNotebook className="w-full h-2/3 mb-1"/>
         睡眠ログ
       </Link>
-      <button className="h-full w-1/2 p-2">
+      <button className="h-full w-1/2 p-2" onClick={ () => setIsAlarmShow(true) }>
         <BsAlarm className="w-full h-2/3 mb-1" />
         アラーム（睡眠記録）
       </button>
+      { isAlarmShow && 
+          <AlarmModal onClick={ hiddenAlarmModal } />
+      }
     </div>
   )
 }
