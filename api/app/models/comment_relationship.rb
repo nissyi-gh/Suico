787701c28d543de2fa2f4136aa66c21ad80fff2d
yuta.onishi <rad6242@icloud.com>
2022-03-11 21:15:07 +0900

    test: comment_relationshipのテストを追加
    テストを追加し、必要なバリデーションをモデルに追加しました。

--- api/app/models/comment_relationship.rb
+++ api/app/models/comment_relationship.rb
@@ -1,4 +1,7 @@
 class CommentRelationship < ApplicationRecord
   belongs_to :sleep_log
   belongs_to :sleep_log_comment
+
+  validates :sleep_log_id, presence: true
+  validates :sleep_log_comment_id, presence: true
 end
--- api/spec/factories/sleep_logs.rb
+++ api/spec/factories/sleep_logs.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :sleep_log do
-    
+    sleep_at { Time.now }
+    wake_at { Time.now }
+    satisfaction { 0.0 }
   end
 end
