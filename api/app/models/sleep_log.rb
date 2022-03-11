bee753cf58620204a001e9aac9dd74b54911e33c
yuta.onishi <rad6242@icloud.com>
2022-03-11 10:54:18 +0900

    fix: seed内のコードを移植
    sleeplog生成コードをsleeplogモデルに移植しました

--- api/app/models/sleep_log.rb
+++ api/app/models/sleep_log.rb
@@ -90,5 +90,52 @@ class SleepLog < ApplicationRecord
       sleep_time_data = sleep_time_analyze(sleep_logs)
       [*data, satisfaction_average(sleep_logs), *sleep_time_data]
     end
+
+    # 就寝と起床が同日になる睡眠データを生成
+    def same_day_sleep_log_creater(user_id, today, iterator, satisfactions)
+      SleepLog.create(
+        user_id: user_id,
+        sleep_at: Time.local(today.year, today.month, today.day, rand(0..1), rand(0..59), 0).in_time_zone - iterator.day,
+        wake_at: Time.local(today.year, today.month, today.day, rand(5..9), rand(0..59), 0).in_time_zone - iterator.day,
+        satisfaction: satisfactions[rand(6)]
+      )
+    end
+
+    # 就寝と起床が別日になる睡眠データを生成
+    def diff_day_sleep_log_creater(user_id, today, iterator, satisfactions)
+      SleepLog.create(
+        user_id: user_id,
+        sleep_at: Time.local(today.year, today.month, today.day, rand(21..23), rand(0..59), 0).in_time_zone - iterator.day,
+        wake_at: Time.local(
+          today.year,
+          today.month,
+          today.day,
+          rand(5..9),
+          rand(0..59),
+          0
+        ).in_time_zone - iterator.day + 1.day,
+        satisfaction: satisfactions[rand(6)]
+      )
+    end
+
+    # 睡眠データを登録
+    def make_sleep_logs(times, user_id)
+      t = Time.zone.now
+      satisfactions = [nil, 0.0, 1.25, 2.5, 3.75, 5.0]
+
+      times.downto(1) do |i|
+        # 0の場合のみ、就寝、起床ともに日付は同じ（夜ふかしパターン）。
+        create_mode = rand(10)
+
+        case create_mode
+        when 0
+          # 日付は同じ（夜ふかしパターン）
+          same_day_sleep_log_creater(user_id, t, i, satisfactions)
+        else
+          # 日付はまたぐ（早寝早起きパターン）
+          diff_day_sleep_log_creater(user_id, t, i, satisfactions)
+        end
+      end
+    end
   end
 end
