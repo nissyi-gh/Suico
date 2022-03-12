74ac6c1af04fb2c48bde6772a309ff6c909e1c62
yuta.onishi <rad6242@icloud.com>
2022-03-12 13:01:15 +0900

    fix: sleep_log_commentに外部キーを追加
    sleep_log_idを外部キーとしました。

--- api/db/migrate/20220311074058_create_sleep_log_comments.rb
+++ api/db/migrate/20220311074058_create_sleep_log_comments.rb
@@ -1,6 +1,7 @@
 class CreateSleepLogComments < ActiveRecord::Migration[6.1]
   def change
     create_table :sleep_log_comments do |t|
+      t.references :sleep_log, foreign_key: true
       t.text :body, null: false
 
       t.timestamps
--- api/db/schema.rb
+++ api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_11_074112) do
+ActiveRecord::Schema.define(version: 2022_03_11_074058) do
 
   create_table "alarm_presets", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.bigint "user_id"
@@ -22,20 +22,12 @@ ActiveRecord::Schema.define(version: 2022_03_11_074112) do
     t.index ["user_id"], name: "index_alarm_presets_on_user_id"
   end
 
-  create_table "comment_relationships", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.bigint "sleep_log_id", null: false
-    t.bigint "sleep_log_comment_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["sleep_log_comment_id"], name: "index_comment_relationships_on_sleep_log_comment_id"
-    t.index ["sleep_log_id", "sleep_log_comment_id"], name: "comment_relationship", unique: true
-    t.index ["sleep_log_id"], name: "index_comment_relationships_on_sleep_log_id"
-  end
-
   create_table "sleep_log_comments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.bigint "sleep_log_id"
     t.text "body", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["sleep_log_id"], name: "index_sleep_log_comments_on_sleep_log_id"
   end
 
   create_table "sleep_logs", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
@@ -59,7 +51,6 @@ ActiveRecord::Schema.define(version: 2022_03_11_074112) do
   end
 
   add_foreign_key "alarm_presets", "users"
-  add_foreign_key "comment_relationships", "sleep_log_comments"
-  add_foreign_key "comment_relationships", "sleep_logs"
+  add_foreign_key "sleep_log_comments", "sleep_logs"
   add_foreign_key "sleep_logs", "users"
 end
