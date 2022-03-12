f854a0af3d98872f210f53dc6a95f32451dad745
yuta.onishi <rad6242@icloud.com>
2022-03-12 11:14:46 +0900

    chore: sleep_log_commentsコントローラーのアクションを設定
    ルーティングにある各アクションを設定しました。

---
+++ api/app/controllers/api/v1/sleep_log_comments_controller.rb
@@ -0,0 +1,20 @@
+module Api
+  module V1
+    class SleepLogCommentsController < ApplicationController
+      def index
+      end
+
+      def create
+      end
+
+      def show
+      end
+
+      def update
+      end
+
+      def destroy
+      end
+    end
+  end
+end
--- api/app/controllers/sleep_log_comments_controller.rb
+++
@@ -1,6 +0,0 @@
-module Api
-  module V1
-    class SleepLogCommentsController < ApplicationController
-    end
-  end
-end
