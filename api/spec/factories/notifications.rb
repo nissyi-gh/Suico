de3c406238fc9484c227b2293410b6981d5f93bd
yuta.onishi <rad6242@icloud.com>
2022-03-14 23:59:25 +0900

    test: notificationにテストを追加
    各項目の存在性などを検証するテストを追加しました。

--- api/spec/factories/notifications.rb
+++ api/spec/factories/notifications.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :notification do
-    
+    title { 'test' }
+    body { 'test' }
   end
 end
--- api/spec/models/notification_spec.rb
+++ api/spec/models/notification_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe Notification, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'モデルの有効性を検証' do
+    before do
+      @user = create(:user)
+    end
+
+    it 'すべてのデータが有れば有効' do
+      expect(build(:notification, user: @user)).to be_valid
+    end
+
+    it 'タイトルがnilなら無効' do
+      expect(build(:notification, user: @user, title: nil)).to be_valid
+    end
+
+    it 'タイトルが空文字なら無効' do
+      expect(build(:notification, user: @user, title: "   ")).to be_valid
+    end
+
+    it 'bodyがnilなら無効' do
+      expect(build(:notification, user: @user, body: nil)).to be_valid
+    end
+
+    it 'bodyが空文字なら無効' do
+      expect(build(:notification, user: @user, body: "   ")).to be_valid
+    end
+
+  end
 end
