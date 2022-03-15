55ba791d5ec30abbd1bf27f1c3ca356ace68624a
yuta.onishi <rad6242@icloud.com>
2022-03-15 20:39:55 +0900

    fix: test内で有効と無効の判定が逆転していた箇所を修正
    testを修正しました。
    notificationにvalidationを追加しました。

--- api/app/models/notification.rb
+++ api/app/models/notification.rb
@@ -1,3 +1,7 @@
 class Notification < ApplicationRecord
   belongs_to :user
+
+  validates :post_user_id, presence: true
+  validates :title, presence: true
+  validates :body, presence: true
 end
--- api/spec/models/notification_spec.rb
+++ api/spec/models/notification_spec.rb
@@ -6,20 +6,28 @@ RSpec.describe Notification, type: :model do
       expect(build(:notification)).to be_valid
     end
 
+    it 'post_user_idが0は有効' do
+      expect(build(:notification, post_user_id: 0)).to be_valid
+    end
+
+    it 'post_user_idがnilなら無効' do
+      expect(build(:notification, post_user_id: nil)).to_not be_valid
+    end
+
     it 'タイトルがnilなら無効' do
-      expect(build(:notification, title: nil)).to be_valid
+      expect(build(:notification, title: nil)).to_not be_valid
     end
 
     it 'タイトルが空文字なら無効' do
-      expect(build(:notification, title: "   ")).to be_valid
+      expect(build(:notification, title: "   ")).to_not be_valid
     end
 
     it 'bodyがnilなら無効' do
-      expect(build(:notification, body: nil)).to be_valid
+      expect(build(:notification, body: nil)).to_not be_valid
     end
 
     it 'bodyが空文字なら無効' do
-      expect(build(:notification, body: "   ")).to be_valid
+      expect(build(:notification, body: "   ")).to_not be_valid
     end
 
   end
