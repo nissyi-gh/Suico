6a268aa66c58040cc8180bbdfc7dd5c7da4994a8
yuta.onishi <rad6242@icloud.com>
2022-03-20 23:46:37 +0900

    feat: notificationの並び順を変更
    created_atの降順で並び替えるように変更しました。

--- api/app/models/notification.rb
+++ api/app/models/notification.rb
@@ -1,6 +1,7 @@
 class Notification < ApplicationRecord
   belongs_to :user
 
+  default_scope { order(created_at: :desc) }
   validates :post_user_id, presence: true
   validates :title, presence: true
   validates :body, presence: true
